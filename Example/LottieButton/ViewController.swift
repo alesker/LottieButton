6716882694fc956bcb0f51575e8fddfdec075554
Ilya Alesker <ilya.alesker@gmail.com>
2017-03-27 12:54:09 +0300

    Initial commit

---
+++ Example/LottieButton/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LottieButton
+//
+//  Created by alesker on 03/27/2017.
+//  Copyright (c) 2017 alesker. All rights reserved.
+//
+
+import UIKit
+import LottieButton
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let button = LottieButton()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ LottieButton/Classes/LottieButton.swift
@@ -0,0 +1,58 @@
+//
+//  LottieButton.swift
+//  Pods
+//
+//  Created by Ilya Alesker on 27/03/2017.
+//
+//
+
+import Foundation
+import SnapKit
+import Lottie
+
+public class LottieButton: UIButton {
+
+    public private(set) var animationView: LOTAnimationView?
+
+    public var animationName: String? {
+        didSet {
+            if let animationView = self.animationView {
+                animationView.removeFromSuperview()
+            }
+            self.animationView = LOTAnimationView(name: animationName)
+
+            if let animationView = self.animationView {
+                self.addSubview(animationView)
+                animationView.snp.makeConstraints { make in
+                    make.edges.equalTo(self.imageView!)
+                }
+                animationView.isHidden = true
+            }
+        }
+    }
+
+    private func blankImage(for image: UIImage) -> UIImage? {
+        UIGraphicsBeginImageContext(image.size)
+        let blankImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return blankImage
+    }
+
+
+    public func playAnimation(withInitialStateImage initialStateImage: UIImage,
+                              andFinalStateImage finalStateImage: UIImage) {
+        let blankImage = self.blankImage(for: initialStateImage)
+        self.setImage(blankImage, for: .normal)
+        self.animationView?.isHidden = false
+
+        self.animationView?.play(completion: { completed in
+            self.animationView?.isHidden = true
+            self.setImage(finalStateImage, for: .normal)
+        })
+    }
+
+    public func playAnimation() {
+        guard let image = self.image(for: .normal) else { return }
+        self.playAnimation(withInitialStateImage: image, andFinalStateImage: image)
+    }
+}
